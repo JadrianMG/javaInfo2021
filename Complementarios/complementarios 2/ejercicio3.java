5364b362e6cf2b75ccdd704f33720b75ca7d4afb
JadrianMG <adrianmelchorgrazziano@gmail.com>
2021-10-15 10:29:19 -0300

    tercer commit

--- Complementarios/complementarios 2/ejercicio3.java
+++ Complementarios/complementarios 2/ejercicio3.java
@@ -1,40 +1,69 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import javax.swing.Spring;
 
 public class ejercicio3 {
   public static void main(String[] args) {
-
-    List<Integer> numeros= new ArrayList<>();
+    List<String> numeros= new ArrayList<>();
     cargarLista(numeros);
-  
+    System.out.println("Baraja: "+numeros);
+    imprimirBarajaInversa(numeros);
+    desordenarBaraja(numeros);
+    //imprimirBaraja(numeros);
+    //System.out.println(numeros);
   }
-
-  public static void cargarLista(List<Integer> numeros) {
-  //Recorre para llenar la lista con números aleatorios   
-
-  for (int i = 0; i < 10; i++) {
-    //numeros.add((int)(Math.random()*100+1));
-    numeros.add(i);
-    /*
-    System.out.print(numeros.get(i)+ "❤ ");
-    System.out.print(numeros.get(i)+ "♢ ");
-    System.out.print(numeros.get(i)+ "♠️ ");
-    System.out.print(numeros.get(i)+ "♧ ");
-    */
-  }
-  
-
-        // Esta línea es la que organiza el ArrayList
-    /*
-        Collections.sort(numeros);
-        System.out.println();
-
-        for (Integer integer : numeros) 
-            System.out.print(integer + " " );
-*/
+  public static void cargarLista(List<String> numeros) {
+    List<String> palo = new ArrayList<>();
+    palo.add("❤ ");
+    palo.add("♢ ");
+    palo.add("♠️ ");
+    palo.add("♧ ");    
+    for (int j = 0; j < 4; j++) {
+      for (int i = 1; i < 14; i++) {
+        //numeros.add(i+ palo.get(j));
+        numeros.add(i+ palo.get(j));  
       }
-}
+    }
+    
+  }
+  public static void imprimirBarajaInversa(List<String> numeros) {
+    List<String> numerosInversos= new ArrayList<>();
+    for (int i = 52; i > 0; i--) {
+      numerosInversos.add(numeros.get(i-1));
+
+    }
+    System.out.println("----------------------------------");
+    System.out.println("Baraja Invertida: "+numerosInversos);
+    }
+
+  public static void desordenarBaraja(List<String> numeros) {
+    String Palo[] = {"❤ ","♢ ","♠️ ","♧ "}; 
+    int cantidad = 52;
+    ArrayList<Baraja> barajas = new ArrayList<Baraja>();
+    while (cantidad > 0) {
+      int numero = ((int) Math.round(Math.random() * 12)) + 1; 
+      int p = (int) Math.round(Math.random() * 3); 
+      boolean existe = false;
+      Baraja baraja = new Baraja(numero, Palo[p]); 
+      for(int i = 0; i < barajas.size(); i++) {
+        if(numero == barajas.get(i).getNumero() && Palo[p] == barajas.get(i).getPalo()) { 
+          existe = true;
+          break; 
+        }
+      }
+      if(!existe) {
+        barajas.add(baraja); 
+        cantidad--;
+      }
+    }
+  System.out.println("Cartas Desordenadas: ");
+  barajas.forEach(baraja -> System.out.print( baraja.getNumero() + " " + baraja.getPalo()+"   "));
+    
+  }
+  }
+
+
 
 
 
--- Complementarios/complementarios 2/prueba.java
+++ Complementarios/complementarios 2/prueba.java
@@ -1,40 +1,59 @@
-import java.util.Collections;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
+class Baraja {
+    private String palo;
+    private int numero;
 
-public class prueba {
-    
-    
-        public static void main(String[] args){
-        ArrayList<Integer> lista = new ArrayList<Integer>();
-        lista.add(5); 
-        lista.add(7); 
-        lista.add(3);
-    
-        Collections.sort(lista);
-    
-        for (Integer numero: lista) {
-            System.out.println(numero);
-        }   
-    
-        
+  public static void Baraja() {
 
-				CopyOnWriteArrayList<Integer> numeros = new CopyOnWriteArrayList<>();
+  }
 
-        //Recorre para llenar la lista con números aleatorios   
-        for (int i = 0; i < 40; i++) {
-            numeros.add((int)(Math.random()*100+1));
-            System.out.print(numeros.get(i)+ "❤ ");
-        }
+  public Baraja(int _numero, String _palo) {
+    this.numero = _numero;
+    this.palo = _palo;
+  }
 
-        // Esta línea es la que organiza el ArrayList
-        Collections.sort(numeros);
-        System.out.println();
+  public String getPalo() {
+    return this.palo;
+  }
 
-        for (Integer integer : numeros) 
-            System.out.print(integer + " " );
+  public int getNumero() {
+    return this.numero;
+  }
 
+  public void setPalo(String _palo) {
+    this.palo = _palo;
+  }
+
+  public void setNumero(int _numero) {
+    this.numero = _numero;
+  }
 }
+
+public class prueba{
+
+  public static void main(String[] args) {
+    String Palo[] = {"Oros","Bastos","Espadas","Copas"}; //Array con el valor de los palos de la baraja
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Diga el número de cartas que desea:");
+    int cantidad = sc.nextInt();
+    ArrayList<Baraja> barajas = new ArrayList<Baraja>();
+    while (cantidad > 0) {
+      int numero = ((int) Math.round(Math.random() * 11)) + 1; // produce números entre 1 y 12 aleatoriamente
+      int p = (int) Math.round(Math.random() * 3); // produce números entre 0 y 3 aleatoriamente
+      boolean existe = false;
+      Baraja baraja = new Baraja(numero, Palo[p]); // Creamos la baraja
+      for(int i = 0; i < barajas.size(); i++) {
+        if(numero == barajas.get(i).getNumero() && Palo[p] == barajas.get(i).getPalo()) { // comparamos la baraja con las ya almacenadas
+          existe = true;
+          break; // Si la baraja ya fue seleccionada no se sigue verificando
+        }
+      }
+      if(!existe) {
+        barajas.add(baraja); // solo añadimos la baraja si es diferente a todas las ya escogidas
+        cantidad--;
+      }
+    }
+  barajas.forEach(baraja -> System.out.println("Baraja: " + baraja.getNumero() + " de " + baraja.getPalo()));
+  }
 }
